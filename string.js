6522dea19f01baddad1f9adddd17b81813366282
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-22 17:21:53 +0500

    array methods

---
+++ string.js
@@ -0,0 +1,60 @@
+let long = ["Usman", "Malik"];
+let length = long.length;
+console.log(length);
+
+var convert = ["String"];
+var string = convert.toString();
+console.log(string)
+
+let At = ["difference", "Mickle"];
+let at = At.at(2);
+console.log(at);
+
+const joining = ["arman ", "arbaz ",];
+const again = ["farhan ", "Fatima "];
+let join = joining.join(again);
+console.log(join);
+
+const popping = ["chain", "ring"]
+popping.pop();
+console.log(popping);
+
+let push = ["boy", "girl"];
+push.push("Man");
+console.log(push);
+
+let shifting = ["number", "numeric"];
+shifting.shift();
+console.log(shifting);
+
+const unshift = ["English", "Roman"];
+unshift.unshift("Arabic");
+console.log(unshift);
+
+const deleting = ["apple", "baanana"];
+var deleted = deleting.delete;
+console.log(deleted)
+
+let concating = ["plus", "minus"];
+let concat = concating.concat("division");
+console.log(concat);
+
+const overRide = ["clothes", "furniture", "Electronics, animals", "human", "birds"];
+const copy = overRide.copyWithin(0,4);
+console.log(copy);
+
+let flatting = [["what"], ["the"], ["flat"], ["do"]];
+let flat = flatting.flat();
+console.log(flat); 
+
+let splicing = ["kiwi", "Mango", "orange", "papaya"];
+let splice = splicing.splice(1,3,"apple");
+console.log(splice);
+
+const toSp = ["ring", "chain", "hand"];
+const toSplice = toSp.toSpliced(3);
+console.log(toSplice);
+
+let slicing = ["slice", "splice", "slicing"];
+let  slice = slicing.slice(2);
+console.log(slice);
